315408421ad58bbcbcd5556f5602b844a83b8175
gusdn7142 <gusdnchl7144@naver.com>
2024-10-28 18:59:06 +0900

    first commit

---
+++ src/test/java/com/example/extract_excel_data_test/ExtractExcelDataTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extract_excel_data_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtractExcelDataTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
